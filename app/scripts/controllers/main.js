73b8f2cab7f1a85dfe6ea5df0c2f63af25ae9bca
docodon <dhsharma27@gmail.com>
2017-01-02 08:44:50 +0530

    set up angular repo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hplannerFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hplannerFrontendApp
+ */
+angular.module('hplannerFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
